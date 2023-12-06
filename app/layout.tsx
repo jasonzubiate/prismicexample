0bbcf2a6d1adfae8a733a5a0e6c257cc9bcb198d
Jason Zubiate <jzubiate.dev@gmail.com>
2023-12-06 10:59:44 -0800

    feature/layout-metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import clsx from "clsx";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { createClient } from "@/prismicio";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -15,10 +16,19 @@ const nunitoSans = Nunito_Sans({
   display: "swap",
 });
 
-export const metadata: Metadata = {
-  title: "Prismic Example",
-  description: "This example should be used to show clients about Prismic CMS",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+
+  const page = await client.getSingle("settings");
+
+  return {
+    title: page.data.site_title || "Prismic CMS Example",
+    description: page.data.meta_description || "Propery of Spinnin Studio",
+    openGraph: {
+      images: [page.data.og_image.url || ""],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
