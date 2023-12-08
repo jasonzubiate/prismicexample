768c842ca55c4448387335799345b45373f7c66a
Jason Zubiate <jzubiate.dev@gmail.com>
2023-12-08 09:14:27 -0800

    feature/footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,18 @@
 import { createClient } from "@/prismicio";
 import Link from "next/link";
+import Bounded from "@/components/Bounded";
 
 async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
   return (
-    <footer>
-      <Link href="/">{settings.data.site_title}</Link>
-      <p>©{new Date().getFullYear()} Spinnin Studio</p>
-    </footer>
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">Imprint</Link>
+        <p>©{new Date().getFullYear()} Spinnin Studio</p>
+      </div>
+    </Bounded>
   );
 }
 
